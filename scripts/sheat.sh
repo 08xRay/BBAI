50788a939c08ea89eeeb3c34dafe58860cb61ec9
Станислав Райцин <mr.steve.ray@gmail.com>
2019-10-20 18:54:21 +0300

    added heat script

--- scripts/sheat.sh
+++ scripts/sheat.sh
@@ -4,6 +4,9 @@ do
 	clear;
 	echo "DEVICE	T°C";
 	echo "=======	====";
-	paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1/' | sed 's/_thermal//' | sed 's/^.* /\U&\E/g';
+	paste <(cat /sys/class/thermal/thermal_zone*/type)\
+		<(cat /sys/class/thermal/thermal_zone*/temp)\
+		| column -s $'\t' -t | sed 's/\(.\)..$/.\1/'\
+		| sed 's/_thermal//' | sed 's/^.* /\U&\E/g';
 	sleep 3;
 done
