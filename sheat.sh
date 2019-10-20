f196bd9ad12daf0864c0b88349b79419aed31e55
Станислав Райцин <mr.steve.ray@gmail.com>
2019-10-20 18:42:02 +0300

    Added sh script to show current heat

---
+++ sheat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while true;
+do
+	clear;
+	echo "DEVICE	T°C";
+	echo "=======	====";
+	paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1/' | sed 's/_thermal//' | sed 's/^.* /\U&\E/g';
+	sleep 3;
+done
